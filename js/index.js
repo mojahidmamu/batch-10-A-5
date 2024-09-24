5bd815d2ad992b01d041c623a5ce65f56ed07a9a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2024-09-24 17:42:31 +0600

    done the donate and history button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
-console.log( 'Hello! Mojahid,,,,add money');
+//  getting all the value
+const accountBalance = parseFloat(document.getElementById('account-balance').value);
+const donationBalance1 = parseFloat(document.getElementById('donation-balance-1').value);
+const donationBalance2 = parseFloat(document.getElementById('donation-balance-2').value);
+const donationBalance3 = parseFloat(document.getElementById('donation-balance-3').value);
+
+// add event listenet for calculate 
+// donateButton.addEventListener('click', function () {
+//   const totalBalance1 = accountBalance - donationBalance1;
+//   const totalBalance2 = accountBalance - donationBalance2;
+//   const totalBalance3 = accountBalance - donationBalance3;
+// })
+
+// history button functionality
+const donateButton =document.getElementById('donation-btn');
+const historyButton = document.getElementById('history-btn');
+historyButton.addEventListener('click', function(){
+  historyButton.classList.add(
+    "bg-[#B4F461]"
+  );
+   donateButton.classList.remove(
+     "bg-[#B4F461]"
+   );
+     document.getElementById('main-section').classList.add('hidden');
+     document.getElementById('history-tab').classList.remove('hidden');
+});
+
+donateButton.addEventListener('click', function(){
+     historyButton.classList.remove(
+       "bg-[#B4F461]"
+     );
+     donateButton.classList.add(
+       "bg-[#B4F461]"
+     );
+     document.getElementById('main-section').classList.remove('hidden');
+     document.getElementById('history-tab').classList.add('hidden');
+});
